0c60a90a6c8647a49c32df1232026d7aadb5af18
Donald Guy <donaldguy@hey.com>
2021-04-03 17:40:25 -0400

    🔨🥄 : Added firefox default zoom tweak, refactored/structured init.lua

---
+++ .hammerspoon/firefox.lua
@@ -0,0 +1,75 @@
+
+local firefox = {
+  appName = 'Firefox Developer Edition',
+
+  keystrokeDelay = 200000,
+  zoomAdjustSnippetF = 'Cc["@mozilla.org/content-pref/service;1"].getService(Ci.nsIContentPrefService2).setGlobal(window.browsingContext.topChromeWindow.FullZoom.name, %.1f, Cu.createLoadContext())'
+}
+
+-- This console is only interactive if `devtools.chrome.enabled` is set to true
+-- in about:config / prefs.js
+local BrowserConsole = {}
+
+function BrowserConsole:new()
+  local app = hs.appfinder.appFromName(firefox.appName)
+  local bc = {}
+
+  app:activate()
+
+  -- we need an activate browser window to get a console;
+  -- if e.g. all windows are minimzed, we need a new one
+  if not app:focusedWindow() then
+    assert(app:selectMenuItem("New Window"), "Couldn't spawn Firefox window to summon console")
+
+    local i = 0
+    repeat
+      app:activate()
+      bc.spawnedBrowser = app:focusedWindow()
+
+      hs.timer.usleep(firefox.keystrokeDelay)
+      i = i + 1
+    until bc.spawnedBrowser or i > 5
+
+    assert(bc.spawnedBrowser)
+  end
+
+  hs.eventtap.keyStroke({"cmd", "shift"}, "j", firefox.keystrokeDelay, app)
+  bc.window = app:focusedWindow()
+  assert(bc.window:title() == "Browser Console")
+
+  self.__index = self
+  return setmetatable(bc, self)
+end
+
+function BrowserConsole:application()
+  return self.window:application()
+end
+
+function BrowserConsole:focus()
+  return self:application():activate(true) and self.window:focus()
+end
+
+function BrowserConsole:close()
+  if self.spawnedBrowser then
+    return self.window:close() and self.spawnedBrowser:close()
+  else
+    return self.window:close()
+  end
+end
+
+function firefox:setDefaultZoom(zoomLevel)
+  local browserConsole = BrowserConsole:new()
+  local clipboardToRestore = hs.pasteboard.getContents()
+
+  hs.pasteboard.callbackWhenChanged(function(_changed)
+    browserConsole:focus()
+    hs.eventtap.keyStroke({"cmd"}, "v", self.keystrokeDelay, browserConsole:application())
+    hs.eventtap.keyStroke({}, hs.keycodes.map["return"], self.keystrokeDelay, browserConsole:application())
+
+    browserConsole:close()
+    hs.pasteboard.setContents(clipboardToRestore)
+  end)
+  hs.pasteboard.setContents(string.format(self.zoomAdjustSnippetF, zoomLevel))
+end
+
+return firefox
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,13 +3,35 @@ hs.loadSpoon("ReloadConfiguration")
 
 uBar = require("uBar")
 contexts = require("contexts")
+firefox = require("firefox")
 
-screenWatcher = hs.screen.watcher.new(function()
-  s = hs.screen.primaryScreen()
-  aspectRatio = (function(mode) return  mode.w / mode.h end)(s:currentMode())
+local ScreenState = {}
 
-  if aspectRatio >= 1.6 and aspectRatio < 1.8  then  -- 16:9 ish
-    s:desktopImageURL('file:///Users/donald/Pictures/wallpapers/16-9/rainbowswoop.png')
+function ScreenState:new()
+  local o = {
+    screen = hs.screen.primaryScreen()
+  }
+  o.mode = o.screen:currentMode()
+
+  local aR = o.mode.w / o.mode.h
+  if aR >= 1.6 and aR < 1.8 then
+    o.aspectRatio = "16:9"
+  elseif aR >= 3.4 and aR < 3.6 then
+    o.aspectRatio = "32:9"
+  end
+
+  o.scale = o.mode.scale
+
+  self.__index = self
+  return setmetatable(o, self)
+end
+
+lastScreenState = ScreenState:new()
+
+handleAspectRatio = {
+  ["16:9"] = (function(screen)
+    print("Swapped to 16:9 screen")
+    screen:desktopImageURL('file:///Users/donald/Pictures/wallpapers/16-9/rainbowswoop.png')
 
     uBar:setSize(uBar.sizes.large)
     uBar:setPosition(uBar.positions.centerFloat)
@@ -17,8 +39,10 @@ screenWatcher = hs.screen.watcher.new(function()
     uBar:relaunch()
 
     contexts:setInactiveSidebarWidth(contexts.inactiveSidebarWidths.iconsAndTitleStart)
-  elseif aspectRatio >= 3.4 and aspectRatio < 3.6 then -- 32:9 ish
-    s:desktopImageURL('file:///Users/donald/Pictures/wallpapers/32-9/rainbowswoop.png')
+  end),
+  ["32:9"] = (function(screen)
+    print("Swapped to 32:9 screen")
+    screen:desktopImageURL('file:///Users/donald/Pictures/wallpapers/32-9/rainbowswoop.png')
 
     uBar:setSize(uBar.sizes.auto)
     uBar:setPosition(uBar.positions.lowerLeft)
@@ -26,7 +50,32 @@ screenWatcher = hs.screen.watcher.new(function()
     uBar:relaunch()
 
     contexts:setInactiveSidebarWidth(contexts.inactiveSidebarWidths.keepExpanded)
+  end)
+}
+
+function handleNewScale(aspectRatio, scale)
+  if aspectRatio == "16:9" and scale == 2.0 then
+    firefox:setDefaultZoom(0.9)
+  elseif aspectRatio == "32:9" and scale == 1.0 then
+    firefox:setDefaultZoom(1.0)
+  elseif aspectRatio == "32:9" and scale == 2.0 then
+    firefox:setDefaultZoom(0.8)
   end
+end
+
+screenWatcher = hs.screen.watcher.new(function()
+  local old = lastScreenState
+  local new = ScreenState:new()
+
+  if old.aspectRatio ~= new.aspectRatio then
+    handleAspectRatio[new.aspectRatio](new.screen)
+  end
+
+  if old.aspectRatio ~= new.aspectRatio or old.scale ~= new.scale then
+    handleNewScale(new.aspectRatio, new.scale)
+  end
+
+  lastScreenState = new
 end)
 
 screenWatcher:start()
@@ -34,4 +83,4 @@ spoon.ReloadConfiguration:start()
 
 i = hs.inspect
 
---hs.console.clearConsole()
+hs.console.setConsole("Config reloaded\n")
