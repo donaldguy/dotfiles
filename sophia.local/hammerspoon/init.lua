56ebe7ae0c32313d25c39a83b8dbfa6ddbe41bbb
Donald Guy <donaldguy@hey.com>
2021-04-05 01:58:42 -0400

    🔨🥄 : Lilith watcher using ping6 shellout instead
    the lua version couldn't deal well with multiple living interfaces

--- sophia.local/hammerspoon/init.lua
+++ sophia.local/hammerspoon/init.lua
@@ -9,56 +9,57 @@ hs.settings = require('hs.settings')
 hs.timer = require('hs.timer')
 lilith = {
   _presenceSetting = "LilithPresent",
-  bridgeIPv6 = "fd9b:e191:f52e:8e49:ce3:f4c9:adfd:1e0d"
+  ethernetIPv6 = "fe80::1c8b:ac07:57b1:8cf6"
 }
 
 function lilith:present()
   return hs.settings.get(self._presenceSetting)
 end
 
-lilith.pingTimer = (function(callback)
-  lilith.pinger = hs.network.ping(lilith.bridgeIPv6):setCallback(callback)
+function interfaceNameFromIPv6Address(targetAddr)
+  for _i, iface in ipairs(hs.network.interfaces()) do
+    adresses =  ((hs.network.interfaceDetails(iface).IPv6 or {}).Addresses or {})
+    for _j, addr in ipairs(adresses) do
+      if addr == targetAddr then
+        return iface
+      end
+    end
+  end
+
+  return nil
+end
+
+
+lilith.pingTimer = (function(resultFn)
+
+  local ts3EthernetInterface = interfaceNameFromIPv6Address("fe80::c13:9999:ab26:fccc")
+
+  local makePinger = function()
+    lilith.pinger = hs.task.new("/usr/local/bin/timeout", resultFn, function(...) return true end, {
+      "5s", --the timeout
+      "/sbin/ping6",
+      "-I", ts3EthernetInterface,
+      "-c", "1",
+      "-n",
+      lilith.ethernetIPv6
+    }):start()
+  end
+  makePinger()
 
   return hs.timer.waitWhile(
     function() return lilith.pinger:isRunning() end,
     function(t)
-      lilith.pinger = hs.network.ping(lilith.bridgeIPv6):setCallback(callback)
+      makePinger()
       t:start()
     end)
-
-end)(function(o, msg, ...)
-  if msg == "didStart" or msg == "receivedPacket" then
-    return
-  elseif msg == "sendPacketFailed" or msg == "didFail" then
-    o:cancel()
-    return
-  end
-  assert(msg == "didFinish")
-
-  local mia = 0
-  for i, packet in ipairs(o:packets()) do
-    if not packet.recv then
-      mia = mia + 1
-    end
-  end
-
-  if mia/o:sent() >= 0.5 then
-    hs.settings.set(lilith._presenceSetting, false)
-  else
-    hs.settings.set(lilith._presenceSetting, true)
-  end
+end)(function(exitCode, ...)
+  hs.settings.set(lilith._presenceSetting, exitCode == 0)
 end)
 
 hs.settings.watchKey("LilithReactor", lilith._presenceSetting, function()
   local lilithPresent = lilith:present()
 
   lilith.pinger:pause()
-  print("pinger paused")
-  local pings_already_sent = lilith.pinger:sent()
-  local total_pings_in_check_when_paused = lilith.pinger:count()
-  if pings_already_sent >= 0.5 * total_pings_in_check_when_paused then
-    lilith.pinger:count(math.min(total_pings_in_check_when_paused * 3, 100)) -- paranoid: don't want bad series of timings growing unbound
-  end
 
   if lilithPresent then
     print("Lilith appeared! ^_^")
@@ -80,7 +81,6 @@ hs.settings.watchKey("LilithReactor", lilith._presenceSetting, function()
   hs.timer.waitUntil(
     function() return hs.network.primaryInterfaces() end,
     function()
-      print("pinger resuming")
        lilith.pinger:resume()
     end
   )
